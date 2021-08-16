01f812f0c60af894bf7bf9be9d6277cae4c3142e
hehecoi222 <hehecoi@outlook.com>
2021-08-16 17:49:50 +0700

    More useful way to comment in JS

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ Not affect anything outside the comment sign
 
 // Even in multi-line
 // comment work on shortcut
+
+//Keep comment works out by classify it and use it WHEN NEEDED
+
+//Ex: To explain something:
+// Here the code is use to reverseString
+
+//Ex: To markup, or TODO: list
+// TODO: Check the result of this code
+// M: This need to refer again in document
+
+//Ex: To block something execute
+// console.log("Hello World!")
