7225836f83b75693c5b2406d5b2e912efd74a79a
hehecoi222 <hehecoi@outlook.com>
2021-08-18 15:20:45 +0700

    Index, length of array

--- index.js
+++ index.js
@@ -6,11 +6,21 @@
 // Length: check its length with arrayName.length
 
 // 2 ways to create array:
-let arr1 = [];
+let arr1 = ["A", true, 2];       //Adding data during creation
 let arr2 = Array(5);   // 5 is the length of array (optional)
 
-arr1[2] = 5;  // Refer to array index
+arr1[2] = 5;  // Refer to array index, adding value after creation
 arr2[1] = 2;
 
-console.log(arr1);
+console.log(arr1.length);
 console.log(arr2);
+
+// Populating array:
+// Add data during array creation: in 1 statement
+// After creation of array: require assigning the value to the index
+// Index already having value will be overwritten
+// M: Keep track of array length
+
+// Length: sum of spaces that have been allocated for values in array
+// Index: Numerates values or spaces in array, starting at 0 to array.length - 1
+// Length always > Index (since index start from 0)
