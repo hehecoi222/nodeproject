44d885cfdf26a3bb22df57094edc41583f7a6b89
hehecoi222 <hehecoi@outlook.com>
2021-08-18 15:08:11 +0700

    Array

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
-// Boolean with switch statement (and other Boolean syntax)
+// Array is list, or collections fo values
+// Can contain different values of different data types
 
-// Implicit false values
-// String: empty string; Object: null; Number: 0
-// Eg:
+// Each value has an index: a unique numerical value represents the value in the array
 
-if (0) {        // Use ! to reverse the result 
-    console.log("0 contain *NO* value")
-};
+// Length: check its length with arrayName.length
 
-// String are case sensitive, and to fix that, convert lower, upper 
-// Or use localeCompare()
+// 2 ways to create array:
+let arr1 = [];
+let arr2 = Array(5);   // 5 is the length of array (optional)
 
-if ("Hello" === "hello") {
-    console.log("Print out something");
-};
+arr1[2] = 5;  // Refer to array index
+arr2[1] = 2;
 
-// Fix #1:
-if (("Hello").toLocaleLowerCase() === "hello") {
-    console.log("Print out something to say " + "hello");
-};
-
-
-// Combinig comparisions
-// &&: and; ||: or
-
-if ((200 === 200) && (3000 === 3000)) { // And
-    console.log("OK!");
-};
-
-if (400 === (400 || 500)) {   // Or
-    console.log("400 or 500 error");
-};
-
-// More useful switch statement:
-
-switch (400) {
-    case 200:       // Always check equality
-        console.log("OK!");
-        break;      // MUST have break if not, the flow falltthrough the entire switch statement;
-    case 400 || 500:    // 400 or 500
-        console.log("Error");
-        break;
-    default:
-        console.log("Unknown");
-        break;
-};    //Just like Golang
+console.log(arr1);
+console.log(arr2);
